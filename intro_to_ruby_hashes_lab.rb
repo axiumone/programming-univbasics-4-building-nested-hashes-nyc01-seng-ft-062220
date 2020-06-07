b819e6bdbf400ae8385ab02e1e1e654aef4538f3
Dmitry Sychev <axiumone@gmail.com>
2020-06-07 17:01:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,11 +15,7 @@ def monopoly_with_second_tier
     :railroads => {}
   }
 
-  monopoly[:railroads] = {
-    :pieces => "4",
-    :rent_in_dollars => {},
-    :names => {}
-  }
+  monopoly[:railroads] = {:pieces => 4}
 
 end
 
